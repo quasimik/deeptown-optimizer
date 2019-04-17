7df74b16225f0209df68a6fc8437a7e5539d7077
Michael Liu <mike666234@gmail.com>
2019-04-16 22:55:29 -0700

    Add optimizer app

---
+++ optimizer/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Resource(models.Model):
+	raw = models.BooleanField(default=False)
+	value = models.IntegerField()
+
+class Recipe(models.Model):
+	product = models.ForeignKey(Resource, on_delete='PROTECT')
+	ingredients = models.ManyToManyField(Resource, through='Ingredient', related_name='recipes')
+	product_qty = models.IntegerField()
+	time_sec = models.IntegerField()
+
+class Ingredient(models.Model):
+	resource = models.ForeignKey(Resource, on_delete='CASCADE', related_name='ingredients')
+	recipe = models.ForeignKey(Recipe, on_delete='CASCADE', related_name='ingredients')
+	qty = models.IntegerField()
