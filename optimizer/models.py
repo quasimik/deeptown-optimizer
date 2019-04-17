a3361c18716be8b014ac949895b403e4a7aad66c
Michael Liu <mike666234@gmail.com>
2019-04-17 00:41:45 -0700

    Add useful resource model methods

--- optimizer/models.py
+++ optimizer/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+RAW_RPM = 6
+RAW_T = 60 / RAW_RPM
+
 # Create your models here.
 class Resource(models.Model):
   name = models.CharField(max_length=50)
@@ -8,6 +11,39 @@ class Resource(models.Model):
 
   def __str__(this):
     return this.name
+
+  def recipe(this):
+    return this.recipe_set.first()
+
+  def recipe_ingreds(this):
+    return this.recipe().ingredient_set.all()
+
+  def raw_value(this):
+    if this.raw:
+      return this.value
+    val = 0
+    for ing in this.recipe_ingreds():
+      val += ing.qty * ing.resource.raw_value()
+    val /= this.recipe().product_qty
+    return val
+
+  def profit(this):
+    return this.value - this.raw_value()
+
+  def time(this):
+    if this.raw:
+      return RAW_T
+    time = this.recipe().time_sec
+    for ing in this.recipe_ingreds():
+      time += ing.qty * ing.resource.time()
+    time /= this.recipe().product_qty
+    return time
+
+  def value_per_time(this):
+    return this.value / this.time()
+
+  def profit_per_time(this):
+    return this.profit() / this.time()
 
 class Recipe(models.Model):
   product = models.ForeignKey(Resource, on_delete='PROTECT')
